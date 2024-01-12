ae9b77aa5b0114e2998e218714c40668ac975513
Peter Tarjan <tp@50.hu>
2024-01-12 17:42:59 +0100

    aoc 21 day 22 ruby code speedup

--- 2021/day22_2.rb
+++ 2021/day22_2.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/ruby
+
+require 'fc'
+
 cmd=[]
 xs=[]
 ys=[]
@@ -9,15 +12,15 @@ while x=gets do
 
   x=x.split("=")
   raise "wtf x" unless x[0]=="x"
-  x=x[1].split("..").map{|a|a.to_i+50}
+  x=x[1].split("..").map &:to_i
 
   y=y.split("=")
   raise "wtf y" unless y[0]=="y"
-  y=y[1].split("..").map{|a|a.to_i+50}
+  y=y[1].split("..").map &:to_i
 
   z=z.split("=")
   raise "wtf z" unless z[0]=="z"
-  z=z[1].split("..").map{|a|a.to_i+50}
+  z=z[1].split("..").map &:to_i
   
   if wut=="on" then val=1 else
     raise "wtf dir" unless wut=="off"
@@ -32,59 +35,62 @@ while x=gets do
   xs << x[1]
   ys << y[0]
   ys << y[1]
-  zs << z[0]
-  zs << z[1]
 
   cmd << [x,y,z,val]
 end
 
 xs=xs.sort.uniq
 ys=ys.sort.uniq
-zs=zs.sort.uniq
 xd={}
 yd={}
-zd={}
 xs.size.times{|i|xd[xs[i]]=i}
 ys.size.times{|i|yd[ys[i]]=i}
-zss=zs.size
-zss.times{|i|zd[zs[i]]=i}
 
-grid=Array.new(xs.size-1) { Array.new(ys.size-1,0) }
-
-
-def dump(z)
-  if z==0 then
-    print "-"
-    return
-  end
-  while z>0 do
-    print z%2
-    z=z>>1
-  end
-end
+grid=Array.new(xs.size-1) { Array.new(ys.size-1,nil) }
 
 cmd.each{|x,y,z,v|
   (xd[x[0]]...xd[x[1]]).each{|xi|
     (yd[y[0]]...yd[y[1]]).each{|yi|
-      if v==1 then
-        grid[xi][yi]=grid[xi][yi]|((1 << zd[z[1]])-(1 << zd[z[0]]))
-      else
-        zi0=zd[z[0]]
-        zi1=zd[z[1]]
-        grid[xi][yi]=grid[xi][yi]&(((((1<<(zss-zi1))-1) << zi1))|((1 << zi0)-1))
+      if grid[xi][yi]==nil then
+        grid[xi][yi]=FastContainers::PriorityQueue.new(:min)
       end
+      grid[xi][yi].push([z[1], grid[xi][yi].size, v],z[0])
     }
   }
 }
 
 sol=0
 grid.each_with_index{|gx,xi|
+  xa=xs[xi+1]-xs[xi]
   gx.size.times{|yi|
-    (zs.size-1).times{|zi|
-      if (gx[yi]&(1 << zi))>0 then
-        sol+=(xs[xi+1]-xs[xi])*(ys[yi+1]-ys[yi])*(zs[zi+1]-zs[zi])
+    if grid[xi][yi]!=nil and grid[xi][yi].size>0 then
+      xya=xa*(ys[yi+1]-ys[yi])
+      t=grid[xi][yi].top_key.to_i
+      curr=FastContainers::PriorityQueue.new(:max)
+      while 1 do
+        while grid[xi][yi]!=nil and grid[xi][yi].size>0 and grid[xi][yi].top_key.to_i==t do
+          curr.push([grid[xi][yi].top[0], grid[xi][yi].top[2]], grid[xi][yi].top[1])
+          grid[xi][yi].pop
+        end
+        if curr.size>0 then
+          tt=curr.top[0]
+          if grid[xi][yi]!=nil and grid[xi][yi].size>0 and grid[xi][yi].top_key.to_i<tt then
+            tt=grid[xi][yi].top_key.to_i
+          end
+        elsif grid[xi][yi]!=nil and grid[xi][yi].size>0 then
+          tt=grid[xi][yi].top_key.to_i
+        else
+          break
+        end
+        if curr.size>0 and curr.top[1]>0 then
+          sol+=xya*(tt-t)
+        end
+        while curr.size>0 and curr.top[0]<=tt do
+          curr.pop
+        end
+        t=tt
       end
-    }
+    end
   }
 }
 p sol
